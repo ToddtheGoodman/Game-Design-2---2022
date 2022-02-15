d8856356ebfe619be9ef8c3e2afc55667cb7d0ef
ToddtheGoodman <84030598+ToddtheGoodman@users.noreply.github.com>
2022-02-15 13:34:34 -0800

    Mouse Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,15 @@ public class PlayerController : MonoBehaviour
 {
 
     public float moveSpeed;
+    public float sprintSpeed;
 
     public CharacterController charCon;
 
     public Vector3 moveInput;
+
+    public Vector2 mouseInput;
+
+    public Transform camTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +28,22 @@ public class PlayerController : MonoBehaviour
         //Assign input for x and z 
         moveInput.x = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         moveInput.z = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        //Assign input for X and Y on the Mouse
+        mouseInput.x = Input.GetAxisRaw("Mouse X");
+        mouseInput.y = Input.GetAxisRaw("Mouse Y");
+
+        //stupid 4th dimensions
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+        camTransform.rotation = Quaternion.Euler(camTransform.rotation.eulerAngles.x + -mouseInput.y, camTransform.rotation.eulerAngles.y, camTransform.rotation.eulerAngles.z);
+        
+        
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveInput.x = Input.GetAxis("Horizontal") * Time.deltaTime * sprintSpeed;
+            moveInput.z = Input.GetAxis("Vertical") * Time.deltaTime * sprintSpeed;
+        }
 
         //This moves the player
         charCon.Move(moveInput);
