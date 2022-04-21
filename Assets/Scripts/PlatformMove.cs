2b28f0b4e8d2114a6283351bd4c3f9c7758ac432
ToddtheGoodman <84030598+ToddtheGoodman@users.noreply.github.com>
2022-04-21 13:20:53 -0700

    Array
    Using Array to create waypoints for moving platform

---
+++ Assets/Scripts/PlatformMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMove : MonoBehaviour
+{
+
+    public Transform[] wayPoints;
+
+    public float moveSpeed;
+
+    public int wayPointIndex;
+
+    public Transform lastWayPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = wayPoints[wayPointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WayPointMove();
+    }
+
+    void WayPointMove()
+    {
+        if (wayPointIndex <= wayPoints.Length -1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, wayPoints[wayPointIndex].position, moveSpeed * Time.deltaTime);
+        }
+
+        if (transform.position == wayPoints[wayPointIndex].position)
+        {
+            wayPointIndex += 1;
+        }
+
+        if (transform.position == lastWayPoint.position)
+        {
+            wayPointIndex = 0;
+        }
+    }
+
+}
